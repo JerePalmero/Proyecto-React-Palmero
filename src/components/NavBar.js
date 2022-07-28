96d0ddc4287c6b5f43c2f2fefb28ea81ddf860e7
Jeremias Palmero <86244225+JerePalmero@users.noreply.github.com>
2022-07-28 15:56:45 -0300

    NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+
+
+function NavBar () {
+    return( 
+        <nav class="nav-cont">
+        
+        
+  <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+     Viajes
+  </a>
+  <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+Nosotros    </a>
+  <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+Contacto    </a>
+      </nav>
+    )
+}
+export default NavBar;

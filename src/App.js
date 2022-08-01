52b5edb52437d03a231b47e47765e0bf0cdf3e90
Jeremias Palmero <86244225+JerePalmero@users.noreply.github.com>
2022-08-01 17:06:29 -0300

    Arreglo de Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from '../src/logo.png';
+
 import './App.css';
 import NavBar from './components/NavBar';
 
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavBar/>
-        <img src={logo} className="App-logo" alt="logo" />
+        
        
         <p className='tit'> Proyecto Agencia de Viaje</p>
       </header>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,30 +1,45 @@
-
+ import logo from '../logo.png';
 
 function NavBar () {
     return( 
         <nav class="nav-cont">
-        
-        
-  <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-     Viajes
-  </a>
-  <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-Nosotros    </a>
-  <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-Contacto    </a>
-      </nav>
-    )
-}
+            <div>
+            <img src={logo} alt="logo" class="logo-nav" width="150px"/>
+            </div>
+            <ul class="cont-nav">
+                <li>
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Viajes
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Nosotros
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Contactos
+                    </a>
+                </li>
+            </ul>
+      
+        </nav>
+      
+    
+          )
+                    }
+
 export default NavBar;
